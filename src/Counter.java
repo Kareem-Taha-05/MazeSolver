2bb1ee550c9a6754252b8e1d623bafe36a888441
Kareem Taha <kmat2040@gmail.com>
2025-06-11 13:39:49 +0300

    Update Counter.java

--- src/Counter.java
+++ src/Counter.java
@@ -1,4 +1,4 @@
-// a counter class to make sure we have the same counter across all classes 
+// a counter class to make sure we have the same counter across all classes (Wrapper class or something idk i failed oop)
 public class Counter {
     public int value;
     public Counter(int value) { this.value = value; }
